abbf6d201b87ed516e5f70e559ffb7a7d91dbe4b
Alexispicard <mellis1@l-sb862e2drj-m.local>
2015-10-06 20:53:12 -0700

    Initial Commit

---
+++ tipCalculatorAlexis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipCalculatorAlexis
+//
+//  Created by Marcus Ellison - Vendor on 10/6/15.
+//  Copyright © 2015 Alexis Picard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
